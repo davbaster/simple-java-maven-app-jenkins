574593bb2131cca2c9fa8ab47ec6227f850de232
davbaster <davbaster@gmail.com>
2023-06-20 09:42:34 -0600

    Update AppTest.java
    fixing some issues with the tests

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,6 +2,8 @@ package com.mycompany.app;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -9,13 +11,14 @@ import org.junit.runner.RunWith;
     features="src/test/resources/feature/manualRunner")
 public class AppTest
 {
-
+    @Test
     public void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
+    @Test
     public void testAppMessage()
     {
         App app = new App();
