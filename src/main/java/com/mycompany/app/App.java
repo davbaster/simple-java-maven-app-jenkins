e7cc4da0adcfc523dbcf05550e1e95c13928c1ca
davbaster <davbaster@gmail.com>
2023-06-20 10:37:03 -0600

    Update App.java
    making more complex the app class

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,33 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
-public class App {
-
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
+public class App 
+{
+    private String message = "Hello World!";
 
     public String getMessage() {
-        return MESSAGE;
+        return this.message;
+    }
+
+    public boolean isMessageLong() {
+        return this.message.length() > 10;
+    }
+
+    public String getMessageWithPrefix(String prefix) {
+        return prefix + this.message;
+    }
+
+    public int getCountOf(char ch) {
+        int count = 0;
+        for (char c : this.message.toCharArray()) {
+            if (c == ch) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String replaceInMessage(String oldStr, String newStr) {
+        return this.message.replace(oldStr, newStr);
     }
 }
+
